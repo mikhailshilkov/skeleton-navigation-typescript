d240465fef5c753ddb251a90bb74c7d20f25ce47
Mikhail Shilkov <mshilkov@astrata.eu>
2016-01-06 12:44:29 +0100

    Repoduce TS2307 error for stackoverflow

---
+++ src/hello.ts
@@ -0,0 +1,5 @@
+export class Hello {
+  sayHello(name:string) : string {
+    return 'Hello ' + name;
+  }
+}
--- src/users.ts
+++ src/users.ts
@@ -1,6 +1,7 @@
 import {autoinject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 import 'fetch';
+import {Hello} from './hello';
 
 @autoinject
 export class Users {
@@ -8,6 +9,7 @@ export class Users {
   users = [];
 
   constructor(private http: HttpClient) {
+    console.log(new Hello().sayHello('Test'));
     http.configure(config => {
       config
         .useStandardConfiguration()
